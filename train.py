5a61580b94ee84e347e332beb3faec4983d227f5
diarimandimby <rdiarimandimby@gmail.com>
2026-08-31 13:35:03 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,13 @@
+import torch
+from torch import nn
+from torch.optim import Adam
+from torchvision import transforms
+from torch.utils.data import DataLoader
 from tqdm import tqdm
-from glob import glob
 import matplotlib.pyplot as plt
 from PIL import Image
+from generator import FastGANGenerator
+from discriminator import ProjectedGANDiscriminator
 
 def transform():
   return transforms.Compose([
@@ -26,20 +32,52 @@ def createData(paths):
   
   tensor_images = torch.stack(list_images)
 
-def dataLoader(dataset, batch_size, shuffle=True):
+def createDataLoader(paths, batch_size, shuffle=True):
   return DataLoader(
-    dataset,
+    createData(paths),
     batch_size=batch_size,
     shuffle=shuffle
   )
 
-def train(n_epochs, data_loader):
-  test_latents = torch.randn(4, 256, 1, 1).to(device)
-  for epoch in range(n_epochs):
-    for i, real_images in enumerate(tqdm(data_loader, desc=f"Epoch {epoch}")):
+def train(
+  n_epochs, 
+  data_loader, 
+  device, 
+  checkpoint_path, 
+  output_res=256, 
+  glr=0.0002, 
+  dlr=0.0002, 
+  betas=(0.0, 0.99), 
+  eps=1e-8, 
+  r=0.8, 
+  pfr=1, 
+  sfr=100, 
+  checkpoint_file=''
+):
+  discriminator = ProjectedGANDiscriminator().to(device)
+  generator = FastGANGenerator(output_res).to(device)
+  opt_G = Adam(generator.parameters(), lr=glr, betas=betas, eps=eps)
+  opt_D = Adam(discriminator.parameters(), lr=dlr, betas=betas, eps=eps)
+  gen_ema = AveragedModel(generator, multi_avg_fn=get_ema_multi_avg_fn(decay=0.999))
 
-      # Définir une variance qui décroît au fil des époques (ex: démarre à 0.2, baisse vers 0)
-      decaying_noise = 0.5 * (0.9999 ** epoch)
+  if(checkpoint_file != ''):
+    checkpoint = torch.load(checkpoint_file)
+    generator.load_state_dict(checkpoint['generator'])
+    discriminator.load_state_dict(checkpoint['discriminator'])    
+    gen_ema.load_state_dict(checkpoint['gen_ema'])
+    opt_G.load_state_dict(checkpoint['opt_G'])
+    opt_D.load_state_dict(checkpoint['opt_D'])
+    k = checkpoint['epoch']
+  else:
+    k = 0
+  
+  test_latents = torch.randn(4, 256, 1, 1).to(device)
+  
+  for epoch in range(n_epochs):
+    # Définir une variance qui décroît au fil des époques (ex: démarre à 0.2, baisse vers 0)
+    decaying_noise = r * (0.9999 ** epoch)
+    
+    for i, real_images in enumerate(tqdm(data_loader, desc=f"Epoch {epoch}"))
 
       # --------------------------------------------
       # Entraînement du Discriminateur
@@ -48,26 +86,19 @@ def train(n_epochs, data_loader):
       opt_D.zero_grad()
 
       if decaying_noise > 0:
-        # On ajoute le bruit uniquement pour le calcul des logits du Discriminateur
         real_images = real_images + torch.randn_like(real_images) * decaying_noise
 
-      real_logits = discriminator(randomGaussianBlur(real_images))
+      real_logits = discriminator(real_images)
 
-      # B. Générer de fausses images et les passer dans l'extracteur figé
-      latent_vectors = torch.randn(batch_size, 256, 1, 1).to(device)  # Vecteur latent du GAN
-
+      latent_vectors = torch.randn(batch_size, 256, 1, 1).to(device)
       with torch.no_grad():
         fake_images = generator(latent_vectors)
 
       if decaying_noise > 0:
-        # On ajoute le bruit uniquement pour le calcul des logits du Discriminateur
         fake_images = fake_images + torch.randn_like(fake_images) * decaying_noise
 
-      fake_logits = discriminator(randomGaussianBlur(fake_images.detach()))
+      fake_logits = discriminator(fake_images.detach())
 
-      del fake_images
-
-      # C. Calcul de la perte et rétropropagation
       Dreal_loss = torch.mean(F.relu(torch.ones_like(real_logits) - real_logits))
       Dgen_loss = torch.mean(F.relu(torch.ones_like(fake_logits) + fake_logits))
 
@@ -77,34 +108,28 @@ def train(n_epochs, data_loader):
 
       opt_D.step()
 
-
       # --------------------------------------------
       # Entraînement du Générateur
       # --------------------------------------------
 
       opt_G.zero_grad()
 
-      latent_vectors = torch.randn(batch_size, 256, 1, 1).to(device)  # Vecteur latent du GAN
+      latent_vectors = torch.randn(batch_size, 256, 1, 1).to(device)
       fake_images = generator(latent_vectors)
 
-      # On réanalyse les fausses images, mais cette fois sans détacher le Générateur
       fake_logits = discriminator(fake_images)
 
-      # Le générateur tente de tromper tous les discriminateurs à la fois
       G_loss = -torch.mean(fake_logits)
 
       G_loss.backward()
       opt_G.step()
-
       gen_ema.update_parameters(generator)
 
-
-    if epoch % 2 == 0:
+    if((epoch + 1) % pfr == 0):
 
       with torch.no_grad():
         sample_images = gen_ema(test_latents)
 
-      # --- BLOC DE DIAGNOSTIC ---
       if 'old_samples' in locals():
         diff = torch.abs(sample_images - old_samples).mean().item()
         print(f"\n[DIAGNOSTIC] Différence numérique réelle avec l'époque précédente : {diff}")
@@ -117,5 +142,15 @@ def train(n_epochs, data_loader):
         axes[ncol].imshow(outputs.cpu().permute(1, 2, 0).numpy())
         axes[ncol].axis('off')
       plt.show()
+
+  if((epoch + 1) % sfr == 0):
+    torch.save({
+      'epoch': epoch + 1,
+      'generator': generator.state_dict(),
+      'discriminator': discriminator.state_dict(),
+      'gen_ema': gen_ema.state_dict(),
+      'opt_G': opt_G.state_dict(),
+      'opt_D': opt_D.state_dict(),
+    }, f'{checkpoint_path}/Faster_pg_checkpoint_epoch_{epoch + 1}.pth')
 
   print(f"g_loss : {G_loss.item()}, dgen_loss : {Dgen_loss.item()}, dreal_loss : {Dreal_loss.item()}")
