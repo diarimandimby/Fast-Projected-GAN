60311cce49602b69b4e0af14705d77266678f850
diarimandimby <rdiarimandimby@gmail.com>
2026-09-03 19:19:30 +0300

    Update train.py

--- train.py
+++ train.py
@@ -138,6 +138,8 @@ def train(
       latent_vectors = torch.randn(batch_size, 256, 1, 1).to(device)
       fake_images = generator(latent_vectors)
 
+      fake_images.requires_grad_()
+
       #fake_images = addRandomGaussianNoise(fake_images, [30, 50])
       #fake_images = addRandomGaussianBlur(fake_images, [0.2, 3])
 
